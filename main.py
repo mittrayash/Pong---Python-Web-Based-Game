76b7039ddeb64b338e6c9e1a788f38e82d0ff31c
mittrayash <mittrayash@gmail.com>
2017-07-03 03:14:00 +0530

    Initialized window size

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 # Implementation of classic arcade game Pong
 # Note: Run this code live at http://www.codeskulptor.org
 # The support for this code is via the codeskulptor interactivity framework which the code relies on.
+
+import simplegui
+import random
+
+# initialize globals - pos and vel encode vertical info for paddles
+WIDTH = 600
+HEIGHT = 400     
